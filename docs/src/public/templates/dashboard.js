d6c7bf0f008bcc2cbdfb917d526c0217e0ff14e1
HsuTing <hsuting0106@gmail.com>
2017-09-02 11:31:20 +0800

    fix dashboard basename

--- docs/src/public/templates/dashboard.js
+++ docs/src/public/templates/dashboard.js
@@ -8,7 +8,11 @@ import router from './../../utils/router';
 
 (() => {
   ReactDOM.render(
-    <Dashboard router={router} />,
+    <Dashboard router={{
+      ...router,
+      basename: `${router.basename}dashboard/`
+    }}
+    />,
     document.getElementById('root')
   );
 })();
