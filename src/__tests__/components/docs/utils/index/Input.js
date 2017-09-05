9e38344a9075bd3badb603897e7622255001ca24
HsuTing <hsuting0106@gmail.com>
2017-09-06 04:51:50 +0800

    prod

--- src/__tests__/components/docs/utils/index/Input.js
+++ src/__tests__/components/docs/utils/index/Input.js
@@ -3,22 +3,28 @@
 import {JSDOM} from 'jsdom';
 
 export default wrapper => {
-  it('## Input', () => {
-    const getBorderColor = () => {
-      return (new JSDOM(wrapper.find('UseReduxWithNoDefaultValue').find('input').html()))
-        .window.document.querySelector('input').style.borderColor;
-    };
+  describe('## Input', () => {
+    it('### add value', () => {
+      const getBorderColor = () => {
+        return (new JSDOM(wrapper.find('UseReduxWithNoDefaultValue').find('Input').html()))
+          .window.document.querySelector('Input').style.borderColor;
+      };
 
-    expect(getBorderColor()).toBe('#000000');
+      expect(getBorderColor()).toBe('#000000');
 
-    wrapper.find('UseReduxWithNoDefaultValue').find('input').simulate('change', {
-      target: {
-        value: 'hsuting0106'
-      }
+      wrapper.find('UseReduxWithNoDefaultValue').find('Input').simulate('change', {
+        target: {
+          value: 'hsuting0106'
+        }
+      });
+
+      expect(getBorderColor()).toBe('#f44336');
+
+      wrapper.find('UseReduxWithNoDefaultValue').find('Button').simulate('click');
     });
 
-    expect(getBorderColor()).toBe('#f44336');
-
-    wrapper.find('UseReduxWithNoDefaultValue').find('Button').simulate('click');
+    it('### test blur', () => {
+      wrapper.find('UseDefaultValue').find('Input').simulate('blur');
+    });
   });
 };
